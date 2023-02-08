db616f56ceb098e65256863b56dfaa4aebbbb3b5
jessicanjos <jessicanjos2003@gmail.com>
2023-02-07 21:23:22 -0300

    Adaptador

--- app/src/main/java/br/com/etecia/pizzaplanet/MainActivity.java
+++ app/src/main/java/br/com/etecia/pizzaplanet/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.pizzaplanet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            idlistView = findViewById(R.id.idlistView);
+            MyAdapter Adapter = new MyAdapter();
+            idlistView.setAdapter(Adapter);
+
+    }
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
