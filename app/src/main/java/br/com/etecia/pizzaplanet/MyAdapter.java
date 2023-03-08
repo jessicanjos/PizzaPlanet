c386594623f8faba61001598c0e441d4e8857384
jessicanjos <jessicanjos2003@gmail.com>
2023-03-07 21:08:59 -0300

    Atualização

---
+++ app/src/main/java/br/com/etecia/pizzaplanet/MyAdapter.java
@@ -0,0 +1,10 @@
+package br.com.etecia.pizzaplanet;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+    }
+}
